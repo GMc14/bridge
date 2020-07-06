ea69428006a4473d79cd7cd7ad6213f99aec5bf1
GMc14 <git@gmc14.com>
2020-07-06 16:25:44 +0100

    Update rules.js

--- rules.js
+++ rules.js
@@ -9,33 +9,46 @@ var handsNeeded;
 var roundWins=0;
 
 function checkLegal(cardID) {
+  
+  console.log("GMcCards-rules.js-checkLegal-#0000");
   if (cardID.charAt(0) == leadSuit) {
+  console.log("GMcCards-rules.js-checkLegal-#0300");
     return true;
   }
   else {
     for (var i=0; i<Player1.length; i++) {
       if (String(Player1[i].suit) == leadSuit) {
         alert("Must Follow Suit!");
+        
+        console.log("GMcCards-rules.js-checkLegal-#0600");
         return false;
       }
     }
+    
+  console.log("GMcCards-rules.js-checkLegal-#1000");
     return true;
   }
+  
 }
 
 function winRound() {
+  console.log("GMcCards-rules.js-winRound-#0000");
   var myCard = $("#myPlay");
   var rightCard = $("#rightPlay");
   var acrossCard = $("#acrossPlay");
   var leftCard = $("#leftPlay");
 
   if (compareCard(myCard,rightCard) && compareCard(myCard, acrossCard) && compareCard(myCard, leftCard)) {
+    
+    console.log("GMcCards-rules.js-winRound-#0300");
     addWin("myStuff");
     roundWins++;
     socketio.emit('winner',{player:playerNum, roomID: roomID});
   }
 
   function compareCard(card1, card2) {
+    
+      console.log("GMcCards-rules.js-compareCard-#0000");
       var card1ID = card1.find("img").attr("id").slice(0,-4);
       var card2ID = card2.find("img").attr("id").slice(0,-4);
       var win = 1;
@@ -50,4 +63,6 @@ function winRound() {
       }
       return win;
   }
+  
+  console.log("GMcCards-rules.js-winRound-#1000");
 }
