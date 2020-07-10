f78524979aa16cee4438de865a04fb13a10a2fbe
GMc <bitbucket@gmc14.com>
2020-07-10 17:00:53 +0100

    deletions

--- setDimension.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  reset_dimensions();
-  // $("#pass").css('right',(($("#bidArea").width()-$("#bidTable").width())/2+$("#pass").width()/2));
-});
-
-function reset_dimensions() {
- // $(".myCards").width(0.71 * $(".myCards").height());
- // $(".otherCards").width(0.71 * $(".otherCards").height());
-}
